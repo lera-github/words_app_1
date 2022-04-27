56b1bbf391ebfd0c8545488fe96efcc959eef0e8
lera-github <leragithub@gmail.com>
2022-04-27 21:27:42 +0300

    плюс Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/pages/profile.dart';
-import 'package:myapp/pages/modules.dart';
+import 'pages/modules.dart';
+import 'pages/profile.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
