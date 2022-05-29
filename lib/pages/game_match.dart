fe30853ce78b06095554680b756e0274c0a39da4
lera-github <leragithub@gmail.com>
2022-05-29 17:34:00 +0300

    d n d полностью работает

--- lib/pages/game_match.dart
+++ lib/pages/game_match.dart
@@ -1,15 +1,19 @@
 import 'dart:math';
 
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:controllable_widgets/controllable_widgets.dart';
+//import 'package:controllable_widgets/controllable_widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:myapp/helpers/drag_drop_match.dart';
+//import 'package:myapp/helpers/drag_drop_match.dart';
 import 'package:myapp/pages/game_match_1.dart';
 import 'package:widget_finder/widget_finder.dart';
 
 List _words1 = []; //массивы слов
 List _words2 = [];
-final myCards = <MyCard>[];
+final myCards = <Widget>[];
+final double _cardSizeX = 90;
+final double _cardSizeY = 60;
+
+final List<Offset> _points = []; // массив позиций карточек
 
 class GameMatch extends StatefulWidget {
   const GameMatch({Key? key, required this.mapdata}) : super(key: key);
@@ -27,6 +31,8 @@ class _GameMatchState extends State<GameMatch> {
 
   bool _offstage = true;
 
+  List<bool> isDropped = [];
+
   // обновление родительского виджета
 /*   _refresh() {
      setState(() {});
@@ -37,7 +43,9 @@ class _GameMatchState extends State<GameMatch> {
     super.initState();
     _words1 = widget.mapdata['words1'] as List;
     _words2 = widget.mapdata['words2'] as List;
+    isDropped = List<bool>.generate(_words1.length * 2, (index) => false);
 
+    getMyCards();
     /* WidgetsBinding.instance?.addPersistentFrameCallback((_) {
       // do something
       debugPrint("Build Completed");
@@ -45,12 +53,20 @@ class _GameMatchState extends State<GameMatch> {
   }
 
 /*   @override
+  void setState(VoidCallback fn) {
+    if (mounted) {
+      _area = MediaQuery.of(context).size;
+      super.setState(fn);
+    }
+  } */
+
+  @override
   void dispose() {
-    _words1.clear();
-    _words2.clear();
+    //_words1.clear();
+    //_words2.clear();
     myCards.clear();
     super.dispose();
-  } */
+  }
 
 /*   Size _getSize() {
     final RenderBox render =
@@ -58,25 +74,27 @@ class _GameMatchState extends State<GameMatch> {
     return render.size;
   } */
 
-  List<MyCard> getMyCards() {
-    double _cardSizeX = 90;
-    double _cardSizeY = 60;
-    List<Offset> _points = []; // массив позиций карточек
+  List<Widget> getMyCards() {
+    _points.clear();
     List<int> _indexListI = []; //массивы перемешанных индексов
     List<int> _indexListJ = [];
-    List<List<double>> _posList = []; //массив координат
+    //final List<List<double>> _posList = []; //массив координат
 
     // если карточки сформированы - выход для обхода перегенерации
-    if (myCards.length == _words1.length * 2) {
-      return myCards;
-    }
+    /* if (myCards.length == _words1.length * 2) {
+      if (myCards.isNotEmpty) {
+        return myCards;
+      }
+    } */
     myCards.clear();
+    _words1 = widget.mapdata['words1'] as List;
+    _words2 = widget.mapdata['words2'] as List;
     // Массивы  индексов перемешанный ВСЕХ слов
     _indexListI = List.generate(_words1.length, (index) => index);
     _indexListJ = List.generate(_words2.length, (index) => index);
     //перемешивание
-    _indexListI.shuffle();
-    _indexListJ.shuffle();
+    //_indexListI.shuffle();
+    //_indexListJ.shuffle();
 /*     for (int i = _indexListI.length - 1; i >= 1; i--) {
       final int j = _random.nextInt(i + 1);
       // обменять значения
@@ -140,7 +158,13 @@ class _GameMatchState extends State<GameMatch> {
       }
       //формирование карточки
       myCards.add(
-        MyCard(
+        TheCard(
+          sizeX: _cardSizeX,
+          sizeY: _cardSizeY,
+          color: i < _words1.length ? Colors.red : Colors.blue,
+          text: _tmptxt,
+        ),
+        /* myCardWidget(
           ind: i,
           pos: _points[i], //_pos
           sizeX: _cardSizeX,
@@ -148,7 +172,7 @@ class _GameMatchState extends State<GameMatch> {
           color: i < _words1.length ? Colors.red : Colors.blue,
           text: _tmptxt,
           //refresh: _refresh,
-        ),
+        ), */
       );
 
       _pos = cardNewPos(_cardSizeX, _cardSizeY);
@@ -160,140 +184,55 @@ class _GameMatchState extends State<GameMatch> {
   //получение случайных координат в области
   Offset cardNewPos(double _sx, double _sy) {
     return Offset(
-      _random.nextDouble() * (_area.width - _sx),
-      _random.nextDouble() * (_area.height - _sy),
+      (_random.nextDouble() * (_area.width - _sx)).abs(),
+      (_random.nextDouble() * (_area.height - _sy)).abs(),
     );
   }
 
-//////////////////////////////////////////////////////////////////////////////
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Offstage(
-        offstage: _offstage,
-        child: WidgetFinder.sizeNotifer(
-          onSizeChanged: (size) {
-            setState(() {
-              _offstage = false;
-            });
-            _area = size as Size;
-          },
-          child: Stack(
-            children: _area == Size.zero ? <Widget>[] : getMyCards(),
-          ),
-          //getControls(),
-        ),
-      ),
-    );
-  }
-}
-/////////////////////////////////////////////////////
-/* 1.On Child Widget : add parameter Function paramter
-class ChildWidget extends StatefulWidget {
-  final Function() notifyParent;
-  ChildWidget({Key key, @required this.notifyParent}) : super(key: key);
-}
-2.On Parent Widget : create a Function for the child to callback
-refresh() {
-  setState(() {});
-}
-3.On Parent Widget : pass parentFunction to Child Widget
-new ChildWidget( notifyParent: refresh );
-4.On Child Widget : call the Parent Function
-  widget.notifyParent(); */
-
-/* class MyNotify extends StatelessWidget {
-  const MyNotify({Key? key, required this.notify, required this.child})
-      : super(key: key);
-  final Function notify;
-  final Widget child;
-  @override
-  Widget build(BuildContext context) {
-    return child;
-  }
-} */
-
-/////////////////////////////////////////////////////
 // виджет карточки
-class MyCard extends StatefulWidget {
-  const MyCard({
-    Key? key,
-    required this.ind,
-    required this.pos,
-    required this.sizeX,
-    required this.sizeY,
-    required this.color,
-    required this.text,
-    //required this.refresh,
-  }) : super(key: key);
-  final int ind;
-  final Offset pos;
-  final double sizeX;
-  final double sizeY;
-  final Color color;
-  final String text;
-  //final Function refresh;
+  Widget myCardWidget({
+    int ind = 0,
+    Offset pos = Offset.zero,
+    double sizeX = 0,
+    double sizeY = 0,
+    Color color = Colors.black,
+    String text = '',
+  }) {
+    return /* Positioned(
+      top: pos.dy,
+      left: pos.dx,
+      child: */
+        TheCard(
+      sizeX: sizeX,
+      sizeY: sizeY,
+      color: color,
+      text: text,
+      // ),
 
-  @override
-  State<MyCard> createState() => _MyCardState();
-}
-
-class _MyCardState extends State<MyCard> {
-  @override
-  Widget build(BuildContext context) {
-    /* final CustomPositionedWidgetController _controller =
-        CustomPositionedWidgetController(
-      offsetBuilder: (contentSize) => Offset(widget.pos.dx, widget.pos.dy),
-      padding: const EdgeInsets.all(4),
-      canGoOffParentBounds: false,
-    ); */
-    return //DragDropItem(key: 'key', value: 'value', dragChild:
-        Positioned(
-      top: widget.pos.dy,
-      left: widget.pos.dx,
-/*       child: GestureDetector(
-        onPanDown: (details) {
-          if (_words1.length * 2 == myCards.length) {
-            final _lastCard = myCards[myCards.length - 1];
-            myCards[myCards.length - 1] = myCards[widget.ind];
-            myCards[widget.ind] = _lastCard;
-          }
-        },
-        onPanUpdate: (details) {
-          final currentBuilder = _controller.offsetBuilder;
-          _controller.offsetBuilder = (Size containerSize) {
-            return currentBuilder.call(containerSize) + details.delta;
-          };
-        }, */
-      /* child: CustomPositionedWidget(
-        key: UniqueKey(),
-        maxSize: Size(widget.pos.dx, widget.pos.dy),
-        controller: _controller, */
-      child: widget.ind < _words1.length * 2
-          ? Draggable<MyCard>(
-              data: myCards[widget.ind],
+////////////////////////////////////////////////////////////////////////////////
+      /* ind < _words1.length * 2
+          ? Draggable(
+              data: myCards[ind],
               feedback: Opacity(
                 opacity: 0.8,
                 child: TheCard(
-                  sizeX: widget.sizeX,
-                  sizeY: widget.sizeY,
-                  color: widget.color,
-                  text: widget.text,
+                  sizeX: sizeX,
+                  sizeY: sizeY,
+                  color: color,
+                  text: text,
                 ),
               ),
               childWhenDragging: Container(),
               child: TheCard(
-                sizeX: widget.sizeX,
-                sizeY: widget.sizeY,
-                color: widget.color,
-                text: widget.text,
+                sizeX: sizeX,
+                sizeY: sizeY,
+                color: color,
+                text: text,
               ),
             )
-          :
-//dragTarget!!!
-          DragTarget<MyCard>(
+          : DragTarget(
               onAccept: (receivedItem) {
-                if (receivedItem.ind == widget.ind) {
+                if (receivedItem  == ind) {
                   setState(() {
                     //item.isAccepted = true;
                   });
@@ -321,17 +260,129 @@ class _MyCardState extends State<MyCard> {
                       ? Colors.green
                       : Colors.transparent,
                   child: TheCard(
-                    sizeX: widget.sizeX,
-                    sizeY: widget.sizeY,
-                    color: widget.color,
-                    text: widget.text,
+                    sizeX: sizeX,
+                    sizeY: sizeY,
+                    color: color,
+                    text: text,
                   ), //item.dropChild,
                 ),
               ),
-            ),
+            ), */
+
+////////////////////////////////////////////////////////////////////////////////////////
     );
   }
+
+//////////////////////////////////////////////////////////////////////////////
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Offstage(
+        offstage: _offstage,
+        child: WidgetFinder.sizeNotifer(
+          onSizeChanged: (size) {
+            setState(() {
+              _offstage = false;
+              _area = size as Size;
+              getMyCards();
+            });
+          },
+          child: Stack(
+            children: dragItems(),
+
+            //dragElements(),
+
+            //myCards, //_area == Size.zero ? <Widget>[] : getMyCards(),
+          ),
+          //getControls(),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> dragItems() {
+    final List<Positioned> _items = [];
+    for (var g = 0; g < _words1.length; g++) {
+      final Positioned _item = Positioned(
+        top: _points[g].dy,
+        left: _points[g].dx,
+        child: Visibility(
+          visible: !isDropped[g],
+          child: Draggable(
+            data: g + _words1.length,
+            feedback: Opacity(
+              opacity: 0.8,
+              child: myCards[g],
+            ),
+            childWhenDragging: Container(),
+            child: myCards[g],
+          ),
+        ),
+      );
+      _items.add(_item);
+    }
+    for (var g = _words1.length; g < _words1.length * 2; g++) {
+      final Positioned _item = Positioned(
+        top: _points[g].dy,
+        left: _points[g].dx,
+        child: Visibility(
+          visible: !isDropped[g],
+          child: DragTarget(
+            builder: (
+              BuildContext context,
+              List<dynamic> accepted,
+              List<dynamic> rejected,
+            ) {
+              return myCards[g];
+            },
+            onWillAccept: (data) {
+              //print(data.toString() + '-' + g.toString());
+              return data == g;
+            },
+            onAccept: (data) {
+              setState(() {
+                isDropped[g] = true;
+                isDropped[g - _words1.length] = true;
+                //print('попал');
+              });
+            },
+          ),
+        ),
+      );
+      _items.add(_item);
+    }
+
+    return _items;
+  }
 }
+
+/////////////////////////////////////////////////////
+/* 1.On Child Widget : add parameter Function paramter
+class ChildWidget extends StatefulWidget {
+  final Function() notifyParent;
+  ChildWidget({Key key, @required this.notifyParent}) : super(key: key);
+}
+2.On Parent Widget : create a Function for the child to callback
+refresh() {
+  setState(() {});
+}
+3.On Parent Widget : pass parentFunction to Child Widget
+new ChildWidget( notifyParent: refresh );
+4.On Child Widget : call the Parent Function
+  widget.notifyParent(); */
+
+/* class MyNotify extends StatelessWidget {
+  const MyNotify({Key? key, required this.notify, required this.child})
+      : super(key: key);
+  final Function notify;
+  final Widget child;
+  @override
+  Widget build(BuildContext context) {
+    return child;
+  }
+} */
+
+/////////////////////////////////////////////////////
 
 class TheCard extends StatelessWidget {
   const TheCard({
