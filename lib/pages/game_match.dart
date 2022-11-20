735d78957c3be5b8acaef619a62f59d005f99830
lera-github <leragithub@gmail.com>
2022-11-20 19:10:00 +0300

    в подборе готовы картинки

--- lib/pages/game_match.dart
+++ lib/pages/game_match.dart
@@ -1,6 +1,9 @@
+import 'dart:js_util';
 import 'dart:math';
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:myapp/helpers/img_hlp.dart';
 import 'package:widget_finder/widget_finder.dart';
 
 //слова
@@ -12,6 +15,7 @@ final myCards = <Widget>[];
 const double cardSizeX = 120; //размер карточки
 const double cardSizeY = 60;
 final List<Offset> _points = []; // массив позиций карточек
+List<Uint8List> _imgsBytes = []; // картинки
 
 class GameMatch extends StatefulWidget {
   const GameMatch({Key? key, required this.mapdata}) : super(key: key);
@@ -28,12 +32,14 @@ class _GameMatchState extends State<GameMatch> {
   var _area = Size.zero; // размер области размещения карточек
 
   bool _offstage = true;
+  // признаки обработанных карточек (совпадение установлено)
+  List<bool> isDropped = [];
 
-  List<bool> isDropped =
-      []; // признаки обработанных карточек (совпадение установлено)
+  //признак показа картинки
+  List<bool> isVisImg = [];
 
   // обновление родительского виджета
-/*   _refresh() {
+  /*   _refresh() {
      setState(() {});
   } */
 
@@ -44,6 +50,7 @@ class _GameMatchState extends State<GameMatch> {
     _words2 = widget.mapdata['words2'] as List;
     _imgs = widget.mapdata['imgs'] as List;
     isDropped = List<bool>.generate(_words1.length * 2, (index) => false);
+    isVisImg = List<bool>.generate(_words1.length * 2, (index) => false);
 
     getMyCards();
   }
@@ -54,12 +61,13 @@ class _GameMatchState extends State<GameMatch> {
     super.dispose();
   }
 
-  List<Widget> getMyCards() {
+  Future<List<Widget>> getMyCards() async {
     _points.clear();
     myCards.clear();
     _words1 = widget.mapdata['words1'] as List;
     _words2 = widget.mapdata['words2'] as List;
     _imgs = widget.mapdata['imgs'] as List;
+    _imgsBytes.clear();
 
     // начальные координаты первой карточки
     var pos = cardNewPos(cardSizeX, cardSizeY);
@@ -114,11 +122,19 @@ class _GameMatchState extends State<GameMatch> {
           sizeY: cardSizeY,
           color: i < _words1.length ? Colors.blue : Colors.green,
           text: tmptxt,
-          imgtag: i.toString() + tmptxt,
+          imgtag: i.toString(),
         ),
       );
       pos = cardNewPos(cardSizeX, cardSizeY);
+      /* if (i < _words1.length) {
+         _imgsBytes.add(
+          await  getImg(
+            getImgName: _imgs[i] as String,
+          ),
+        );
+      } */
     }
+    _imgsBytes = await getImgs(getImgsName: _imgsBytes, isLoaded: false);
     return myCards;
   }
 
@@ -134,7 +150,7 @@ class _GameMatchState extends State<GameMatch> {
   List<Widget> dragItems() {
     final List<Positioned> items = [];
     //
-    for (var g = 0; g < _words1.length; g++) {
+    /* for (var g = 0; g < _words1.length; g++) {
       final Positioned item = Positioned(
         top: _points[g].dy,
         left: _points[g].dx,
@@ -155,7 +171,7 @@ class _GameMatchState extends State<GameMatch> {
         ),
       );
       items.add(item);
-    }
+    } */
     //
 
     //for (var g = _words1.length; g < _words1.length * 2; g++) {
@@ -185,6 +201,26 @@ class _GameMatchState extends State<GameMatch> {
                   ),
                 ),
                 childWhenDragging: Container(),
+                /* onDragUpdate: (details) {
+                  isVisImg[g] = true;
+                }, */
+                onDragStarted: () {
+                  if (!isVisImg[g]) {
+                    /*Future.delayed(
+                      const Duration(milliseconds: 2000),
+                      () { */
+                    setState(() {
+                      isVisImg[g] = true;
+                    });
+                    /*},
+                    ); */
+                  }
+                },
+                onDragEnd: (_) {
+                  setState(() {
+                    isVisImg[g] = false;
+                  });
+                },
                 child: Visibility(
                   visible: !isDropped[g],
                   child: myCards[g],
@@ -214,7 +250,61 @@ class _GameMatchState extends State<GameMatch> {
         ),
       );
       items.add(item);
+      //
+
+      //пробуем разложить изображения ===============================================
+      /* final Positioned itemImg = Positioned(
+        top: _points[g].dy + 20,
+        left: _points[g].dx + 30,
+        child: Visibility(
+          visible: isDropped[g],
+          child: SizedBox(
+            height: 30,
+            child: Image.asset('logo1.png'),
+          ),
+        ),
+      );
+
+      //
+      items.add(itemImg); */
     }
+
+    // картинки положим сверху
+    for (var g = 0; g < _words1.length * 2; g++) {
+      /* return Offset(
+      (_area.width - cardSizeX)
+      (_random.nextDouble() * (_area.height - sy)).abs(), */
+      double sx = _points[g].dx - 200;
+      double sy = _points[g].dy - 160;
+      if (_points[g].dx < _area.width / 2) {
+        sx = _points[g].dx + cardSizeX;
+      }
+      if (_points[g].dy < _area.height / 2) {
+        sy = _points[g].dy + cardSizeY;
+      }
+      final Positioned item = Positioned(
+        top: sy,
+        left: sx,
+        child: Visibility(
+          visible: isVisImg[g],
+          child: SizedBox(
+            height: 160,
+            width: 200,
+            child: Center(
+              child: /* Image.memory(
+                _imgsBytes[g % _words1.length],
+                fit: BoxFit.cover,
+              ), */
+                  ImageLoader(
+                imgName: _imgs[g % _words1.length] as String,
+              ),
+            ),
+          ),
+        ),
+      );
+      items.add(item);
+    }
+
     return items;
   }
 
@@ -247,7 +337,7 @@ class _GameMatchState extends State<GameMatch> {
 }
 
 // карточка
-class TheCard extends StatefulWidget {
+class TheCard extends StatelessWidget {
   const TheCard({
     Key? key,
     required this.sizeX,
@@ -263,128 +353,83 @@ class TheCard extends StatefulWidget {
   final String imgtag;
 
   @override
-  State<TheCard> createState() => _TheCardState();
-}
-
-class _TheCardState extends State<TheCard> {
-  @override
   Widget build(BuildContext context) {
-    double _animatedContainerHeight = 100;
-    double _animatedContainerWidth = 100;
-    return MouseRegion(
-      /* onEnter: (_) => _showImg(
-        context,
-        imgtag,
-        'logo1.png',
-      ), */
-      /* child: Hero(
-        tag: imgtag, */
-      onEnter: (value) {
-        setState(() {
-          _animatedContainerHeight = 100;
-          _animatedContainerWidth = 150;
-        });
-      },
-      onExit: (value) {
-        setState(() {
-          _animatedContainerHeight = 100;
-          _animatedContainerWidth = 100;
-        });
-      },
-      cursor: SystemMouseCursors.click,
-      child: AnimatedContainer(
-        duration: Duration(milliseconds: 200),
-        height: _animatedContainerHeight,
-        width: _animatedContainerWidth,
-        child:
-
-            //
-            Card(
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      shadowColor: Colors.blueAccent,
+      elevation: 8,
+      child: ClipPath(
+        clipper: ShapeBorderClipper(
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
+            borderRadius: BorderRadius.circular(10),
           ),
-          shadowColor: Colors.blueAccent,
-          elevation: 8,
-          child: ClipPath(
-            clipper: ShapeBorderClipper(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-            ),
-            child: Container(
-              height: widget.sizeY, //size
-              width: widget.sizeX,
-              decoration: BoxDecoration(
-                border:
-                    Border(left: BorderSide(color: widget.color, width: 10)),
-                color: Colors.yellowAccent.shade100,
-              ),
-              padding: const EdgeInsets.all(2.0),
-              alignment: Alignment.centerLeft,
-              child: AutoSizeText(
-                widget.text,
-                maxLines: 5,
-                wrapWords: false,
-                style: const TextStyle(
-                  fontSize: 14,
-                  fontStyle: FontStyle.italic,
-                ),
-                overflow: TextOverflow.ellipsis,
-                minFontSize: 8,
-              ),
-            ),
+        ),
+        child: Container(
+          height: sizeY, //size
+          width: sizeX,
+          decoration: BoxDecoration(
+            border: Border(left: BorderSide(color: color, width: 10)),
+            color: Colors.yellowAccent.shade100,
           ),
-          //),
-          //),
+          padding: const EdgeInsets.all(2.0),
+          alignment: Alignment.centerLeft,
+          child: AutoSizeText(
+            text,
+            maxLines: 5,
+            wrapWords: false,
+            style: const TextStyle(
+              fontSize: 14,
+              fontStyle: FontStyle.italic,
+            ),
+            overflow: TextOverflow.ellipsis,
+            minFontSize: 8,
+          ),
         ),
       ),
     );
   }
-}
 
-void _showImg(BuildContext context, String _tag, String _imgname) {
-  //heroed = true;
-  final Orientation _orientation = MediaQuery.of(context).orientation;
-  Navigator.of(context).push(
-    MaterialPageRoute(
-      builder: (ctx) => Scaffold(
-        body: SafeArea(
-          child: Center(
-            /* child: Hero(
-                tag: _imgname, */
-            child: Flex(
-              direction: _orientation == Orientation.landscape
-                  ? Axis.horizontal
-                  : Axis.vertical,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: GestureDetector(
-                    onTap: () {
-                      //heroed = false;
-                      Navigator.of(context).pop();
-                    },
-                    child: DecoratedBox(
-                      decoration: BoxDecoration(
-                        borderRadius: const BorderRadius.all(
-                          Radius.circular(10.0),
-                        ),
-                        image: DecorationImage(
-                          fit: BoxFit.contain,
-                          image: AssetImage(
-                            _imgname,
-                          ),
-                        ),
+  /* void _showImg(BuildContext context, String _tag, String _imgname) {
+    //heroed = true;
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => Scaffold(
+          body: /* SafeArea(
+            child: Center(
+              child: */
+              SizedBox(
+            height: 200,
+            width: 160,
+            child: Hero(
+              tag: _imgname,
+              child: GestureDetector(
+                onTap: () {
+                  //heroed = false;
+                  Navigator.of(context).pop();
+                },
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(10.0),
+                    ),
+                    image: DecorationImage(
+                      fit: BoxFit.contain,
+                      image: AssetImage(
+                        _imgname,
                       ),
                     ),
                   ),
-                )
-              ],
+                ),
+              ),
             ),
-            //),
           ),
+          /*    ),
+          ), */
         ),
       ),
-    ),
-  );
+    );
+  } */
 }
